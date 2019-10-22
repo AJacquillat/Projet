e0c3ef0153bb34bd9981ba006fc8ee164d4f88c4
Louis <louisperrotin@gmail.com>
2019-10-22 15:41:30 +0200

    fin de prochain point

--- Programme.py
+++ Programme.py
@@ -26,23 +26,31 @@ def find_seed(g, c=0, eps=2**-26):
 #    return (x-0.25)**5
 #
 #print(find_seed(g))
+def f(x,y):
+    return np.sin(x)+2*np.sin(y)
 
 def grad_f(x,y):
     g=autograd.grad
     return np.r_[g(f,0)(x,y),g(f,1)(x,y)]
 
-def prochain_point(f,c=0,delta=0.01,x,y):
+def prochain_point(f,x,y,delta=0.01):
     grad=grad_f(x,y)
     if grad[1]!=0:
         d_phi=-grad[0]/grad[1]
         delta2=delta/sqrt(1+d_phi**2)
-        return (x+delta2, y+d_phi)
-        
+        return (x+delta2, y+d_phi*delta2)
+    elif grad[0]!=0:
+        d_phi=-grad[1]/grad[0]
+        delta2=delta/sqrt(1+d_phi**2)
+        return (x+d_phi*delta2,y+delta2)
+    else:
+        return None
         
     
-#def simple_contour(f, c=0, delta=0.01):
-#    t=find_seed(f,c)
-#    if t==None:
-#        return ([],[])
-#    else:
-#        
+def simple_contour(f, c=0, delta=0.01):
+    t=find_seed(f,c)
+    if t==None:
+        return ([],[])
+    else:
+        les_x=t[0]
+        les_y=t[1]
